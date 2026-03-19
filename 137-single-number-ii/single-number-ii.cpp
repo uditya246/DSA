1d1ec6ce5702b1c218c4811f3f9ea3e8ac339e1b
uditya246 <udityachinurao@gmail.com>
2026-03-19 17:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (98.84%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0;i<32;i++){
-            int cnt = 0;
-            for(auto it : nums){
-                if(it&(1<<i)){
-                    cnt++;
-                }
-            }
-            if(cnt%(3)) ans = ans|(1<<i);
+        // int ans = 0;
+        // for(int i = 0;i<32;i++){
+        //     int cnt = 0;
+        //     for(auto it : nums){
+        //         if(it&(1<<i)){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt%(3)) ans = ans|(1<<i);
+        // }
+        // return ans;
+
+
+        //use concept of buckets
+        int ones = 0;
+        int twos = 0;
+        for(int i = 0;i<nums.size();i++){
+            ones = (ones^nums[i])&(~twos);
+            twos = (twos^nums[i])&(~ones);
         }
-        return ans;
+        return ones;
     }
 };
