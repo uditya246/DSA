56d86c095d1b7e9350c06c95f65dfc462091550b
uditya246 <udityachinurao@gmail.com>
2026-03-21 12:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (26.34%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,12 +6,14 @@ public:
         int l = 0;
         int r = 0;
         while(r<nums.size()){
-            if(nums[r]==0) zeros++;
-            while(zeros>k){
+            if(nums[r]==0){
+                zeros++;
+            }
+            if(zeros>k){
                 if(nums[l]==0) zeros--;
                 l++;
             }
-            ans = max(ans,r-l+1);
+            if(zeros<=k)ans = max(ans,r-l+1);
             r++;
         }
         return ans;
