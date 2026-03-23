168c098646177c0095731b4608ef7c2f2b13c8fa
uditya246 <udityachinurao@gmail.com>
2026-03-23 14:22:47 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (84.82%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum = 1;
+        int i=1;
+        int n = ratings.size();
+        while(i<ratings.size()){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int peak =1;
+            while(i<n&&ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down = 1;
+            while(i<n&&ratings[i]<ratings[i-1]){
+                
+                sum+=down;
+                down++;
+                i++;
+            }
+            if(down>peak){
+                sum+=(down-peak);
+            }
+        }
+        return sum;
+
+    }
+};
