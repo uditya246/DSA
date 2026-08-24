0d97e089941ebd5b554589d86f05c6e55014f335
uditya246 <udityachinurao@gmail.com>
2026-08-24 12:21:43 +0530

    Time: 115 ms (33.24%) | Memory: 104.7 MB (11.13%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int maxdiff(int i, vector<int>& s, vector<int>& dp) {
-        if (i == s.size() - 1)
-            return s[i];
-
-        if (dp[i] != INT_MIN)
-            return dp[i];
-
-        int next = maxdiff(i + 1, s, dp);
-
-        return dp[i] = max(next, s[i] - next);
+    int maxdiff(int i,vector<int>& s,vector<int>&dp){
+        if(i==s.size()-1) return s[i];
+        if(dp[i]!=INT_MIN) return dp[i];
+        int x = max(maxdiff(i+1,s,dp),s[i]-maxdiff(i+1,s,dp));
+        return dp[i] = x;
     }
-
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
-
-        vector<int> s(n);
+        vector<int>s(n);
         s[0] = stones[0];
+        for(int i = 1;i<n;i++){
+            s[i] = s[i-1]+stones[i];
+        }
+        vector<int>dp(n,INT_MIN);
 
-        for (int i = 1; i < n; i++)
-            s[i] = s[i - 1] + stones[i];
-
-        vector<int> dp(n, INT_MIN);
-
-        return maxdiff(1, s, dp);
+        return maxdiff(1,s,dp);
     }
 };
